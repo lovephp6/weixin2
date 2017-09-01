cc7614c1e875a11602e48d28b29317b26046ba5d
lovephp6 <649448500@qq.com>
2017-09-01 08:31:59 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,7 @@
                 </div>
 
                 <div class="links">
+		    <a>this is a test</a>
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
