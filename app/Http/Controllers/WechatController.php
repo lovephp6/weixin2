b995687dcc84f199ec522fd698ccd364ee145d71
十夜66 <649448500@qq.com>
2017-09-01 08:58:10 +0800

    register weixin

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -6,8 +6,19 @@ use Illuminate\Http\Request;
 
 class WechatController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return 2222;
+        // 将timestamp, nonce, token按字典序排序
+        $timestamp = $request['timestamp'];
+        $nonce = $request['nonce'];
+        $token = lovephpforweixin;
+        $signature = $request['signature'];
+        $arr = [$timestamp, $nonce, $signature];
+        sort($arr);
+        $info = explode('',$arr);
+        dd($info);
+        // 将排序后的三个参数拼接之后用sha1加密
+
+        //将加密后的字符串与signature进行对比,判断该请求是否来自微信
     }
 }
