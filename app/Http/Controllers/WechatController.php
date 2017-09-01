706e5d83798fb6c2f20fdb7d38442d985333fa91
十夜66 <649448500@qq.com>
2017-09-01 09:43:58 +0800

    response message

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -27,7 +27,7 @@ class WechatController extends Controller
             exit;
         } else {
             // 1.获取微信推送过来的post数据(xml格式)
-            $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+            $postArr = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : file_get_contents("php://input");
             // 2. 处理消息类型,并设置回复类型和内容
             $postObj = simplexml_load_string($postArr);
             // 3. 判断该数据包是否是订阅事件推送
