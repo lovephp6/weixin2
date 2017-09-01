b33c9c2c9eb5e3ad732713c7062646ffa74349c8
十夜66 <649448500@qq.com>
2017-09-01 10:13:20 +0800

    response message

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -8,32 +8,6 @@ class WechatController extends Controller
 {
     public function index(Request $request)
     {
-        // 将timestamp, nonce, token按字典序排序
-        $timestamp = $request['timestamp'];
-        $nonce = $request['nonce'];
-        $echostr = $request['echostr'];
-        $token = 'lovephpforweixin';
-        $signature = $request['signature'];
-        $arr = [$timestamp, $nonce, $token];
-        sort($arr);
-
-        // 将排序后的三个参数拼接之后用sha1加密
-        $str = sha1(implode('',$arr));
-
-        //将加密后的字符串与signature进行对比,判断该请求是否来自微信
-        if ($str == $signature && $echostr) {
-            echo $echostr;
-            exit;
-        } else {
-            $this->responseMsg();
-        }
-    }
-
-    /**
-     *  接收事件推送并回复
-     */
-    public function responseMsg()
-    {
         // 1.获取微信推送过来的post数据(xml格式)
         $postArr = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : file_get_contents("php://input");
 
@@ -58,6 +32,6 @@ class WechatController extends Controller
                 return $info;
             }
         }
-
     }
+
 }
